24163208f1f63954c8e2b5ba2423c4f332dc15fd
Yannick Biheul <yannickbiheul@outlook.fr>
2020-05-01 11:43:32 +0200

    Initialisation du projet

---
+++ index.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Yannick Biheul</title>
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/normalize.css">
+    <!-- POLICES -->
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <script src="script.js" defer></script>
+    
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="nav1">
+                <div class="bouton">
+                    <span class="span1"></span>
+                    <span class="span2"></span>
+                    <span class="span3"></span>
+                </div>
+            </div>
+            <div class="nav2">
+                <a href="#">Accueil</a>
+                <a href="#">Présentation</a>
+                <a href="#">Projets</a>
+                <a href="#">Contact</a>
+            </div>
+            <div class="nav3">
+                <a href="">Connexion</a>
+            </div>
+        </nav>
+        <div class="nav-mobile">
+            <div class="nav-mobile1">
+
+            </div>
+            <div class="nav-mobile2">
+                <a href="#">Accueil</a>
+                <a href="#">Présentation</a>
+                <a href="#">Projets</a>
+                <a href="#">Contact</a>
+            </div>
+            <div class="nav-mobile3">
+                <a href="">Connexion</a>
+            </div>
+        </div>
+    </header>
+
+    <main>
+
+    </main>
+
+    
+</body>
+</html>
