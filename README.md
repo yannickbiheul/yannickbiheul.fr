c9ff223232a45ac47ffeddbe1b94072d69c2e411
Yannick Biheul <yannickbiheul@outlook.fr>
2020-05-01 17:46:41 +0200

    Création écran d'accueil

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
 # yannickbiheul.fr
 
-Un site pour me présenter
+Un site pour me présenter en 4 sections :
+
+* Accueil
+* Présentation
+* Projets
+* Contact
 
 # Responsive Design
 
-3 breakpoints : 600px, 992px et 1200px.
+5 breakpoints en largeur : 600px, 769px, 992px, 1024px, 1200px.
+
+1 breakpoint en hauteur: 630px.
+
+Fonctionne en mode portrait et paysage sur :
+* Samsung Galaxy S5, S7, A20E,
+* Moto G4,
+* Pixel 2, Pixel 2 XL,
+* IPhone 5/SE, IPhone 6,7,8,
+* IPhone 6,7,8 Plus,
+* IPhone X,
+* IPad et IPad Pro.
 
 # Langages
 
@@ -13,17 +29,11 @@ Un site pour me présenter
 * JavaScript
 * PHP
 
-# 4 Sections
-
-* Accueil
-* Présentation
-* Projets
-* Contact
-
 # Journal 2020
 
 * **1er Mai**
     * Création du menu de navigation.
+    * Création de l'écran d'accueil.
 
 # Outils
 
--- index.php
+++ index.php
@@ -9,47 +9,29 @@
     <link rel="stylesheet" href="css/normalize.css">
     <!-- POLICES -->
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Bangers&display=swap" rel="stylesheet">
     <script src="script.js" defer></script>
-    
 </head>
 <body>
-    <header>
-        <nav>
-            <div class="nav1">
-                <div class="bouton">
-                    <span class="span1"></span>
-                    <span class="span2"></span>
-                    <span class="span3"></span>
-                </div>
-            </div>
-            <div class="nav2">
-                <a href="#">Accueil</a>
-                <a href="#">Présentation</a>
-                <a href="#">Projets</a>
-                <a href="#">Contact</a>
-            </div>
-            <div class="nav3">
-                <a href="">Connexion</a>
-            </div>
-        </nav>
-        <div class="nav-mobile">
-            <div class="nav-mobile1">
+    <?php include('header.php'); ?>
 
+    <section id="main">
+        <section id="main1">
+            <div class="nom">
+                <h1>Yannick Biheul</h1>
             </div>
-            <div class="nav-mobile2">
-                <a href="#">Accueil</a>
-                <a href="#">Présentation</a>
-                <a href="#">Projets</a>
-                <a href="#">Contact</a>
+            <div class="phrase">
+                <p>Développement web</p>
             </div>
-            <div class="nav-mobile3">
-                <a href="">Connexion</a>
-            </div>
-        </div>
-    </header>
+            <div class="liens">
+                <a href="">CV</a>
+                <a href="#">GitHub</a>
+            </div>   
+        </section>
 
-    <main>
-
+        <section id="main2">
+            <img src="images/web5.png" alt="Web">
+        </section>
     </main>
 
     
