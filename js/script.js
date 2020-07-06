a2bdef3f3c1a3c3d65996a2ffcb9f76afa4cd9c7
Yannick Biheul <yannickbiheul@outlook.fr>
2020-07-06 11:03:00 +0200

    Ajout menu Titre

--- js/script.js
+++ js/script.js
@@ -4,3 +4,11 @@ let menuBurger = document.querySelector('.menuBurger');
 boutonBurger.addEventListener('click', function() {
     menuBurger.classList.toggle('menuBurgerOpen');
 })
+
+let titre = document.querySelector('.titre');
+let menuTitre = document.querySelector('.menuTitre');
+
+titre.addEventListener('click', function() {
+    menuTitre.classList.toggle('menuTitreOpen');
+    titre.classList.toggle('titreOpen');
+})
