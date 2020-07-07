f442ef97b1b87e259f9c85961f1dec3b3caffeb9
Yannick Biheul <yannickbiheul@outlook.fr>
2020-07-07 10:27:40 +0200

    Finition page d'accueil

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
-let boutonBurger = document.querySelector('.boutonBurger');
-let menuBurger = document.querySelector('.menuBurger');
-
-boutonBurger.addEventListener('click', function() {
-    menuBurger.classList.toggle('menuBurgerOpen');
-})
-
+/* BOUTON BURGER */
 let titre = document.querySelector('.titre');
 let menuTitre = document.querySelector('.menuTitre');
+let span1 = document.querySelector('.span1');
+let span2 = document.querySelector('.span2');
+let span3 = document.querySelector('.span3');
 
 titre.addEventListener('click', function() {
     menuTitre.classList.toggle('menuTitreOpen');
     titre.classList.toggle('titreOpen');
+    span1.classList.toggle('span1Open');
+    span2.classList.toggle('span2Open');
+    span3.classList.toggle('span3Open');
 })
