197f1c50b181b1ae699584c8b559d4c58fd2d9e1
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-01 22:43:19 +0200

    Refactoring JavaScript

--- js/script.js
+++ js/script.js
@@ -1,42 +1,22 @@
+// MENU BURGER
 let boutonBurger = document.querySelector('.boutonBurger');
 let menuBurger = document.querySelector('.menuBurger');
+
 let span1 = document.querySelector('.span1');
 let span2 = document.querySelector('.span2');
 let span3 = document.querySelector('.span3');
 
-let boutonAccueil = document.querySelector('.boutonAccueil');
-let boutonPresentation = document.querySelector('.boutonPresentation');
-let boutonPortfolio = document.querySelector('.boutonPortfolio');
-let boutonContact = document.querySelector('.boutonContact');
+let elts = document.querySelectorAll('.elt');
 
-boutonBurger.addEventListener('click', function() {
+function toggleMenu() {
     menuBurger.classList.toggle('menuBurgerOpen');
     span1.classList.toggle('span1Open');
     span2.classList.toggle('span2Open');
     span3.classList.toggle('span3Open');
-})
+}
 
-boutonAccueil.addEventListener('click', function() {
-    menuBurger.classList.toggle('menuBurgerOpen');
-    span1.classList.toggle('span1Open');
-    span2.classList.toggle('span2Open');
-    span3.classList.toggle('span3Open');
-})
-boutonPresentation.addEventListener('click', function () {
-    menuBurger.classList.toggle('menuBurgerOpen');
-    span1.classList.toggle('span1Open');
-    span2.classList.toggle('span2Open');
-    span3.classList.toggle('span3Open');
-})
-boutonPortfolio.addEventListener('click', function () {
-    menuBurger.classList.toggle('menuBurgerOpen');
-    span1.classList.toggle('span1Open');
-    span2.classList.toggle('span2Open');
-    span3.classList.toggle('span3Open');
-})
-boutonContact.addEventListener('click', function () {
-    menuBurger.classList.toggle('menuBurgerOpen');
-    span1.classList.toggle('span1Open');
-    span2.classList.toggle('span2Open');
-    span3.classList.toggle('span3Open');
-})
+boutonBurger.addEventListener('click', toggleMenu);
+
+for (let i = 0; i < elts.length; i++) {
+    elts[i].addEventListener('click', toggleMenu);
+}
