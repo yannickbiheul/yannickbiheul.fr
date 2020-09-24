a68ca78e77fdcefd1e34d6043a9605dd92fe1904
Yannick Biheul <yannickbiheul@outlook.fr>
2020-09-24 17:08:55 +0200

    Menu Burger terminé

---
+++ js/yannick.js
@@ -0,0 +1,19 @@
+// MENU BURGER
+let boutonBurger = document.querySelector(".boutonBurger");
+let menuBurger = document.querySelector(".menuBurger");
+let span1 = document.querySelector(".span1");
+let span2 = document.querySelector(".span2");
+let span3 = document.querySelector(".span3");
+let elts = document.querySelectorAll(".elt");
+
+function ouvrirMenu() {
+    menuBurger.classList.toggle("menuBurgerOpen");
+    span1.classList.toggle("span1Open");
+    span2.classList.toggle("span2Open");
+    span3.classList.toggle("span3Open");
+}
+boutonBurger.addEventListener("click", ouvrirMenu);
+
+for (let i = 0; i < elts.length; i++) {
+    elts[i].addEventListener("click", ouvrirMenu);
+}
